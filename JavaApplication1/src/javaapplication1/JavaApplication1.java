d47361472ec0c55fe654ac00431e6d4d46b6cb48
AleezaViray <aleeza.viray@gmail.com>
2016-09-15 08:20:25 +0800

    adds containsfalse

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -15,10 +15,15 @@ public class JavaApplication1 {
             return str.matches("[tT]rue");
     }
     
+    public static boolean containsFalse(String str){
+            return str.matches(".*false.*");
+    }
+    
     
     public static void main(String[] args) {
         
         System.out.println(isTrue("true"));
+        System.out.println(containsFalse("kushfdskjdgf"));
     }
     
 }
